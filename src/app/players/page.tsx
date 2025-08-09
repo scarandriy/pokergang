f29834d1b33b13bcca3dfeccc57b23d5f3585451
scarandriy <scarandry@gmail.com>
2025-08-09 04:16:19 +0200

    fixed layout of players apge t oalign with the rest of the pages

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -42,10 +42,8 @@ export default function PlayersPage() {
   });
 
   return (
-    <div className="p-4 sm:p-8">
-      <div className="flex items-center justify-between mb-4 sm:mb-6">
-        <h1 className="text-xl sm:text-2xl font-bold">Игроки</h1>
-      </div>
+    <div className="p-4">
+  
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-6">
         {playerStats.map(({ player, totalSessions, totalResults, totalBuyins, description }) => (
